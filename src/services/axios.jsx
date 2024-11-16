3c7423735e7271edc1d5ba5a3369e2bf6137e6f4
Shaun <nhlamulosmashaba@gmail.com>
2024-11-16 09:55:31 +0200

    updated urls

--- src/services/axios.jsx
+++ src/services/axios.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL||"https://carrentalbackend-0zuw.onrender.com/api/",
+  baseURL: import.meta.env.VITE_API_URL||"https://carrentalbackend-0zuw.onrender.com/api",
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
