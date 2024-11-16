acab0cade3277ea9ebc0deee324516e00a9f42ef
Shaun <nhlamulosmashaba@gmail.com>
2024-11-16 09:44:14 +0200

    changed vite url

--- src/services/refreshToken.jsx
+++ src/services/refreshToken.jsx
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL||"/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL||"https://carrentalbackend-0zuw.onrender.com";
 
 const refreshToken = async () => {
   const refresh_token = localStorage.getItem('refresh_token');
